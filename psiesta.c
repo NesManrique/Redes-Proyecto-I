62d01e12e0e73ab200c99994bb116d7f13e483d4
Nocturn <nes.manrique@gmail.com>
2012-02-03 19:01:18 -0430

    FUNCIONAAAAA =D

--- psiesta.c
+++ psiesta.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -11,37 +12,34 @@
 
 /* Macro variables */
 #define NCLIENTS 5
-#define PAGE "/datos.html"
-#define USERAGENT "WebHTML"
 
 /* Global variable */
-char *query;
-char *serverip, *serverport, *localport, *time;
+int listensock, b_psiesta, s_psensor;
+int sent, got, tmp, i, status,pid,flag, flag2;
+char recvbuf[1024];
+char sendbuf[1024];
+char query[1024];
+char *serveraddr, *serverport, *localport, *time;
+char serverip[20];
 char archv[15];
-FILE *point;
+struct in_addr inaddr;
+struct hostent* host;
+struct sockaddr_storage data ;
+struct sockaddr_in server, address;
+FILE *file, *point, *cache;
+
 
 /******************************************/
 /* Helper functions */
 
-void error_handler(char * msg){
-    fprintf(stderr, "web server: %s\n", msg);
-    exit(EXIT_FAILURE);
-}
+void make_query(char *host, char *page){
 
-char * make_query(char *host, char *page){
-
-    char * message  = "\nGET %s HTTP/1.0\r\nHost: %s\r\nUser-agent: %s\r\n\r\n";
-    query = (char *)malloc(strlen(host) + strlen(page) + strlen(message) +
-                           strlen(USERAGENT));
-    sprintf(query,message, page, host, USERAGENT);
+    char * message  = "\nGET /%s HTTP/1.0\r\nHost: %s\r\nConnection: keep-alive\r\n\r\n";
+    sprintf(query,message, page, host);
     
-    return query;
 }
 
-/******************************************/
-
-void main(int argc, char *argv[]){
-
+void parser(int argc, char *argv[]){
     switch(argc){
         case 2:
             if(!strcmp(argv[1],"-help")){
@@ -60,7 +58,19 @@ void main(int argc, char *argv[]){
         case 9:
             if(!strcmp(argv[1],"-h") && !strcmp(argv[3],"-p")
                     && !strcmp(argv[5],"-l") && !strcmp(argv[7],"-t")){
-                serverip = argv[2];
+                serveraddr = argv[2];
+                if(inet_aton(serveraddr, &inaddr))
+                    host = gethostbyaddr((char *)&inaddr, sizeof(inaddr), AF_INET);
+                else
+                    host = gethostbyname(serveraddr);
+
+                if(!host){
+                    herror("Error looking up host\n");
+                    exit(1);
+                }
+
+                sprintf(serverip,"%s",inet_ntoa(*(struct in_addr *) host->h_addr));
+                printf("Psensor Host: %s\n", host->h_name);
                 serverport = argv[4];
                 localport = argv[6];
                 time = argv[8];
@@ -72,229 +82,324 @@ void main(int argc, char *argv[]){
             break;
     }
 
+}
+
+/******************************************/
+
+void main(int argc, char *argv[]){
+
+    parser(argc,argv);
+
     printf("IP %s\nServPort %s\nLocalPort %s\nTime %s\n", serverip,
             serverport, localport, time);
 
-        
-    /* Code Server */
-    int listensock, b_psiesta;
-    int sent, tmp, i;
-    char recvbuf[BUFSIZ+1];
-    char sendbuf[BUFSIZ+1];
-    char * endstr;
-    char *aux;
-    struct sockaddr_storage data ;
-    struct sockaddr_in server, address;
-    struct in_addr inaddr;
-    struct hostent* host;
-    FILE *file;
-    int sock;
-    
     memset(sendbuf,0,sizeof(sendbuf));
     memset(archv,0,sizeof(archv));
     memset(recvbuf,0,sizeof(recvbuf));
     memset(&server,0,sizeof(server));
+    memset(&address,0,sizeof(address));
 
-    /* Creating socket between server and client */
-    if ((listensock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        error_handler("Error creating server socket...\n");
+    /* This child updates the files  */
+    pid = fork();
+    if(pid<0){
+        perror("Error forking: \n");
+        close(listensock);
+        exit(1);
 
-    /* Populate the struct */
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY ;
-    server.sin_port = htons(atoi(localport));  /* temporally */
+    }else if(pid==0){
+        close(listensock);
+        char nextfile[15];
+        if((point = fopen("Files", "w+")) == NULL)
+            perror("Error opening the cache file: \n");
 
-    //Binding the socket
-    if(bind(listensock, (struct sockaddr * ) &server, sizeof(server)) < 0)
-        error_handler("Binding socket not created...\n");
-       
-    //Put the socket to listen
-    if (listen(listensock, NCLIENTS) < 0)
-        error_handler("listen call failed\n");
+        //Fill the structure with host data
+        address.sin_family = AF_INET; 
+        address.sin_port = htons(atoi(serverport));
+        inet_pton(AF_INET,serverip,&(address.sin_addr));
 
-    //Loop for receiving request from clients
-    socklen_t size_data; 
-    size_data = sizeof(size_data);
-
-
-    /* this child updates the files  */
-    if(!fork()){
-        char c[15];
-        sleep(atoi(time));
         while(1){
-            if((point = fopen(aux, "r")) == NULL)
-                perror("fopen: ");
+            rewind(point);
+            sleep(atoi(time));
 
-            while(fscanf(point,"%s",c)){ 
+            while(fscanf(point,"%[^\n]\n",nextfile)!=EOF){
+
+                //Create socket for this child
+                if((s_psensor=socket(AF_INET, SOCK_STREAM, 0))<0)
+                    perror("Error creating file manager child: \n");
+
                 //Send query again 
-                if(connect(sock, (struct sockaddr *)&address, sizeof(struct sockaddr)) < 0){
-                    printf("connection not established");
-                    //El servidor esta caido, busco en la cache y si no tengo
-                    //el archivo devuelvo un msj 
+                if(connect(s_psensor, (struct sockaddr *)&address, sizeof(struct sockaddr))<0){
+                    //The server psensor is down so this child continues with
+                    //next file
+                    printf("No hubo conexion\n");
+                    continue;
                 }
-
-                //El servidor esta arriba sobreescribo la informacion
-                //y la mando al browser
 
                 //Build the query 
                 char ip[20];
-                inet_ntop(AF_INET,&(address.sin_addr),ip,20);
-                sprintf(ip,"%s%s",ip,serverport);
-                printf("ip %s\n\n",ip);
-                query = make_query(ip, archv);
+                sprintf(ip,"%s%s%s",serverip,":",serverport);
+                //printf("ip %s\n\n",ip);
+                make_query(ip, nextfile);
                 printf("\nQUERY: \n<BEGIN>%s<END>\n",query);
-                fprintf(point, "%s", archv); 
 
                 //Send the query to server. If there's something that has not been
                 //sent, the rest is sent.
-
-                while (sent < strlen(query)){
-                    tmp = send(sock, query, strlen(query) - sent , 0);
-                    if(tmp == -1){
-                        error_handler("Error sending query..."); 
-                    }
+                sent =0;
+                while(sent < strlen(query)){
+                    tmp = send(s_psensor, &(query[sent]), strlen(query) - sent , 0);
+                    if(tmp<0)
+                        perror("Error sending query");
 
                     sent += tmp;
                 }
 
-                memset(recvbuf, 0, sizeof(recvbuf));
-                if ((file = fopen(archv,"w+")) == NULL)
-                    error_handler("Error opening file... ");
+                printf("\nnextfile %sc\n",nextfile);
+
+                memset(recvbuf, 0, 1024);
+                if ((file = fopen(nextfile,"w")) == NULL)
+                    perror("Error opening file: \n");
 
                 // Receiving the data 
-                while((tmp = recv(sock, recvbuf, BUFSIZ, 0)) > 0 ){
-                    fwrite(recvbuf, sizeof(char) ,sizeof(recvbuf), file);
-                    perror("");
-                    memset(recvbuf, 0, tmp); 
+                while((tmp = recv(s_psensor, recvbuf, 1023, 0)) > 0 ){
 
-                } 
+                    printf("tmp %d\n",tmp);
+                    fwrite(recvbuf, sizeof(char) ,tmp, stdout);
 
-                if(tmp < 0){
-                    error_handler("Error receiving data...");
+                    fwrite(recvbuf, sizeof(char) ,tmp, file);
+                    memset(recvbuf, 0, tmp);
                 }
 
-                free(query); 
-                close(sock);
+                if(tmp < 0){
+                    perror("Error receiving data: \n");
+                }
 
+                memset(query,0,sizeof(query));
+                fclose(file);
+                close(s_psensor);
             }
         }
+
+        printf("Exit hijo file manager\n");
+        exit(0);
     }
 
+    /* Code Server */
 
-    while (1){
+    /* Creating socket between server and client */
+    if ((listensock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("Error creating server socket: \n");
+        exit(1);
+    }
 
-        b_psiesta = accept(listensock,(struct sockaddr *)&data ,&size_data); 
-        if (b_psiesta == -1)
-            error_handler("Error accepting connection ...\n");
+    /* Populate the struct */
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(atoi(localport)); 
 
-        inet_ntop(data.ss_family,
-                (void *)(&(((struct sockaddr_in *)((struct sockaddr *)&data))->sin_addr)),
-                recvbuf, sizeof(recvbuf));
-        printf("server: got connection from %s\n", recvbuf);
+    //Binding the socket
+    if(bind(listensock, (struct sockaddr * ) &server, sizeof(server)) < 0){
+        perror("Binding socket not created");
+        exit(1);
+    }
 
-        memset(recvbuf,0,sizeof(recv));
+    //Put the socket to listen
+    if (listen(listensock, NCLIENTS) < 0){
+        perror("Listen call failed");
+        exit(1);
+    }
 
-        if(!fork()){ // This is the child process
+    socklen_t size_data; 
+    size_data = sizeof(size_data);
+
+    //Loop for receiving request from clients
+    while(1){
+
+        b_psiesta = accept(listensock,(struct sockaddr *)&data, &size_data);
+        if (b_psiesta < 0){
+            perror("Error accepting connection from browser");
+            exit(1);
+        }
+
+        //Childs to attend browsers requests
+        pid = fork();
+        if(pid<0){
+            perror("Error forking");
             close(listensock);
+            close(b_psiesta);
+            exit(1);
+        }
+        if(pid==0){
+            close(listensock);
+            // Receiving the data from browser 
+            tmp = recv(b_psiesta, &recvbuf[got], 1023, 0);
+            printf("tmp recv browser %d\n",tmp);
+            fwrite(&recvbuf[got], sizeof(char) ,tmp, stdout);
 
-            while((tmp=recv(b_psiesta,recvbuf,BUFSIZ,0))<0){
-                perror("");
+            if(tmp<0)
+                perror("Error receiving data");
+    
+            memset(archv,0,15);
+
+            //Parsing the file to retrieve from psensor
+            for(i=5;recvbuf[i]!=' ';i++){
+                archv[i-5]=recvbuf[i];
             }
 
-            endstr = strstr(&recvbuf[4]," ");            
+            archv[i-5]='\0';
 
-            for(i=4;recvbuf[i]!=endstr[0];i++){
-                archv[i-4]=recvbuf[i];
-            }
-
-            printf("archv %s\n", archv);
-
-            sent = 0;
-
-            if(inet_aton(serverip, &inaddr))
-                host = gethostbyaddr((char *)&inaddr, sizeof(inaddr), AF_INET);
+            i=1; //File archv exists
+            
+            if((file = fopen(archv,"r")) == NULL)
+                i = 0; //If archv doesnt exists
             else
-                host = gethostbyname(serverip); 
+                fclose(file);
+            
 
-            printf("IP: %s\n", inet_ntoa(*(struct in_addr *) host->h_addr));
-            printf("Host: %s\n", host->h_name);
-
-            if(!host){
-                herror("Error looking up host\n");
-                exit(1);
-            }
-
-            //Create socket
-            if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-                error_handler("listening socket not created");
+            printf("i %d\n\n", i);
 
             //Fill the structure with host data
             address.sin_family = AF_INET; 
             address.sin_port = htons(atoi(serverport));
-            memcpy(&address.sin_addr , host->h_addr_list[0], sizeof(address.sin_addr));
+            inet_pton(AF_INET,serverip,&(address.sin_addr));
 
-            //Connect with host 
-            if(connect(sock, (struct sockaddr *)&address, sizeof(struct sockaddr)) < 0){
-                printf("connection not established");
-                //El servidor esta caido, busco en la cache y si no tengo
-                //el archivo devuelvo un msj 
+            //Create socket for this child
+            if((s_psensor=socket(AF_INET, SOCK_STREAM, 0))<0)
+                perror("Error creating file manager child");
+
+            //Connecting psensor 
+            if(connect(s_psensor, (struct sockaddr *)&address, sizeof(struct sockaddr))<0){
+                //If psensor is not active search cache memory
+                //If we got the archive send it to browser
+                //If we dont, send a message
+                printf("No connection to server: \n");
+                exit(1);
             }
 
-            //El servidor esta arriba sobreescribo la informacion
-            //y la mando al browser
-
-            //Build the query 
+            //Make query for psensor
             char ip[20];
-            inet_ntop(AF_INET,&(address.sin_addr),ip,20);
-            sprintf(ip,"%s%s",ip,serverport);
-            printf("ip %s\n\n",ip);
-            query = make_query(ip, archv);
+            sprintf(ip,"%s%s%s",serverip,":",serverport);
+            make_query(ip, archv);
             printf("\nQUERY: \n<BEGIN>%s<END>\n",query);
-            fprintf(point, "%s", archv); 
 
-            //Send the query to server. If there's something that has not been
-            //sent, the rest is sent.
+            //Send query to psensor
+            tmp = send(s_psensor, query, strlen(query), 0);
+            printf("tmp send psensor %d\n\n",tmp);
+            if(tmp<0)
+                perror("Error sending query");
 
-            while (sent < strlen(query)){
-                tmp = send(sock, query, strlen(query) - sent , 0);
-                if(tmp == -1){
-                    error_handler("Error sending query..."); 
+            memset(recvbuf, 0, 1024);
+            
+            printf("archv %s c\n",archv);
+
+            //Receiving the data and overwriting the file
+            flag=1;
+            flag2=1;
+            while((tmp = recv(s_psensor, recvbuf, 1023, 0))>0){
+
+                printf("tmp recv psensor %d\n\n",tmp);
+                fwrite(recvbuf, sizeof(char) ,tmp, stdout);
+
+                printf("flag %d - code %d\n", flag , atoi(&recvbuf[9]));
+
+                if(flag && 404!=atoi(&recvbuf[9])){
+                    //If first loop and no problems recving
+                    //open file archv
+                    if((file = fopen(archv,"w")) == NULL)
+                        perror("Error opening file");
+
+                    printf("i adentro %d\n", i);
+                    if(!i){
+                        //i=0 if file doesnt exists
+                        //If file doesnt exists, write in the cache directorie
+                        printf("i adentro ***  %d\n", i);
+                        if((cache = fopen("Files","a"))==NULL)
+                            perror("Error opening file");
+
+                        if(fwrite(archv, sizeof(char), strlen(archv), cache)!=strlen(archv))
+                            printf("Error writing in file");
+                        if(fputc('\n',cache)<0)
+                            printf("Error writing char in file");
+
+                        fclose(cache);
+                    }
+
+                    fwrite(recvbuf, sizeof(char), tmp, file);
+                    memset(recvbuf, 0, tmp);
+                    flag2=0; //flag2=0 no problems in first chunk of data
                 }
 
-                sent += tmp;
+                if(!flag && !flag2){
+                    //Store next chuncks knowing its not the first loop
+                    //and there was no problem with the first
+                    printf("PRUEBA \n");
+                    fwrite(recvbuf, sizeof(char), tmp, file);
+                    memset(recvbuf, 0, tmp);
+                }
+
+                flag=0;
+                
             }
-
-            memset(recvbuf, 0, sizeof(recvbuf));
-            if ((file = fopen(archv,"w+")) == NULL)
-                error_handler("Error opening file... ");
-
-            // Receiving the data 
-            while((tmp = recv(sock, recvbuf, BUFSIZ, 0)) > 0 ){
-                fwrite(recvbuf, sizeof(char) ,sizeof(recvbuf), file);
-                perror("");
-                memset(recvbuf, 0, tmp); 
-
-            } 
 
             if(tmp < 0){
-                error_handler("Error receiving data...");
+                perror("Error receiving data from psensor");
             }
 
-            free(query); 
-            close(sock);
+
+            printf("HOLAAAAAAA\n");
+            if(!flag2)
+                fclose(file);
 
 
-            if(send(b_psiesta,"Hello, World!" ,13,0) == -1)
-                error_handler("Error sending...\n");
+            printf("aqui\n");
+            //perror("");
 
+            if(flag2==1){
+                //Psensor is up but file its not found 
+                //Send to browser
+    
+                char response[60] = "Psensor was active but requested file was not found\n";
+                printf("hola %s\n",response);
+                tmp = send(b_psiesta, response, strlen(response), 0);
+
+                if(tmp<0)
+                    perror("Error sending response to browser");
+
+                printf("chao tmp %d\n",tmp);
+
+            }else if(flag2==0){
+                //Psensor is up and file was found
+                if((file = fopen(archv,"r")) == NULL)
+                    perror("Error opening file");
+                
+                char line[1024];
+                printf("Entre en flag2\n");
+
+                //Send from file to browser
+                //while(fscanf(file,"%[^\n]\n",line)!=EOF){ 
+                    fread(line,sizeof(char), sizeof(line), file);
+                    tmp = send(b_psiesta,line,strlen(line),0);
+                    printf("linewidth %d, tmp send to browser %d\n",(int)strlen(line),tmp);
+                    if(tmp<0)
+                        perror("Error sending response to browser");
+                //}
+
+                fclose(file);
+            }
 
             close(b_psiesta);
-            exit(0);           
-
+            close(listensock);
+            exit(0);
         }
-
         close(b_psiesta);
     }
     
-}
+    //signal(pid,SIGTERM); //Kill the childs 
 
+    close(listensock);
+
+    //if (b_psiesta < 0)
+    //    perror("Error accepting connection from browser: \n");
+
+    //wait(&status);
+    
+}
